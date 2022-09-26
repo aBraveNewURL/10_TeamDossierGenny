225eb3948e07fc3b827490db3500301258a0e816
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-09-26 14:51:39 -0600

    updated lib, employee

---
+++ __tests__/employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/employee');
+const newEmployee = new Employee('Damien','666','theOmen@fakeemail.com')
+
+test 
+('tests whether or not we can create a new employee NAME properly with Employee constructor', () => {
+    expect().toBe();
+    expect().toBe();
+    expect().toBe();
+});
