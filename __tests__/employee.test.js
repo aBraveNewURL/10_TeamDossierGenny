4e5611c1ef101486dbffe3f867d91fd5d1bc1e1e
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-09-26 15:25:12 -0600

    updated employee test, employee

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,9 +1,25 @@
 const Employee = require('../lib/employee');
-const newEmployee = new Employee('Damien','666','theOmen@fakeemail.com')
+const newEmployee = new Employee('Damien',666,'theOmen@fakeemail.com')
 
-test 
-('tests whether or not we can create a new employee NAME properly with Employee constructor', () => {
-    expect().toBe();
-    expect().toBe();
-    expect().toBe();
+it('tests whether or not we can create a new employee NAME properly with Employee constructor', () => {
+    expect(employee.name).toBe('Damien');
+    expect(employee.id).toBe(666);
+    expect(employee.email).toBe('theOmen@fakeemail.com');
 });
+
+it('tests the getName method', () => {
+    expect(employee.getName()).toBe('Damien')}
+    );
+
+it('tests the getId method', () => {
+    expect(employee.getId()).toBe(666)}
+    );
+
+it('tests the getEmail method', () => {
+    expect(employee.getEmail()).toBe('theOmen@fakeemail.com ')}
+    );
+
+it('tests the getRole method', () => {
+    expect(employee.getRole()).toBe('Employee')}
+    );
+
